15688ec9c42b04b6b549346ee456ad4454dd6564
Jiale-Lu <lulujiale@outlook.com>
2018-07-05 04:54:49 +0100

    record vedio

--- code/video_test.py
+++ code/video_test.py
@@ -2,16 +2,30 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,200)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,130)
+fw = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+fh = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (fw,fh))
 
 while(cap.isOpened()):
     ret, frame = cap.read()
+    if ret==True:
+        frame = cv2.flip(frame,0)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # write the flipped frame
+        out.write(frame)
 
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
 
+# Release everything if job is finished
 cap.release()
+out.release()
 cv2.destroyAllWindows()
-
