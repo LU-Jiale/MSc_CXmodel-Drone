88c01f7d1c665ecbb34c0f8d9cbf57ec8f855061
Jiale <lulujiale@outlook.com>
2018-07-07 09:12:19 +0000

    cameara_calibration

---
+++ code/undistorte.py
@@ -0,0 +1,41 @@
+import cv2
+import sys
+import numpy as np
+
+DIM=(648, 486)
+#K=np.array([[246.35274583696963, -4.826428559211498, 323.93183074769644], [0.0, 243.4716435505204, 243.24494508428336], [0.0, 0.0, 1.0]])
+D=np.array([[-0.08634557881311974], [0.09465735860807668], [0.05719051783268446], [-0.005709516540189177]])
+
+
+K=np.array([[206.26480624709637, 0.0, 323.5], [0.0, 206.26480624709637, 242.5], [0.0, 0.0, 1.0]])
+#D = np.array([[ 0.00001], [ 0.00001], [ 0.00001], [ 0.00001]])
+
+
+def undistort(img_path, balance=0.0, dim2=None, dim3=None):
+
+    img = cv2.imread(img_path)
+    print(img.shape)
+    dim1 = img.shape[:2][::-1]  #dim1 is the dimension of input image to un-distort
+
+    assert dim1[0]/dim1[1] == DIM[0]/DIM[1], "Image to undistort needs to have same aspect ratio as the ones used in calibration"
+
+    if not dim2:
+        dim2 = dim1
+
+    if not dim3:
+        dim3 = dim1
+
+    scaled_K = K * dim1[0] / DIM[0]  # The values of K is to scale with image dimension.
+    scaled_K[2][2] = 1.0  # Except that K[2][2] is always 1.0
+
+    # This is how scaled_K, dim2 and balance are used to determine the final K used to un-distort image. OpenCV document failed to make this clear!
+    new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(scaled_K, D, dim2, np.eye(3), balance=balance)
+    map1, map2 = cv2.fisheye.initUndistortRectifyMap(scaled_K, D, np.eye(3), new_K, dim3, cv2.CV_16SC2)
+    undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+
+    cv2.imshow("undistorted", undistorted_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    undistort(sys.argv[1], float(sys.argv[2]))
--- code/video_test.py
+++ code/video_test.py
@@ -1,33 +1,41 @@
 import numpy as np
 import cv2
+import os
 
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,200)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT,130)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,648)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,486)
 fw = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 fh = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
+print("Frame size: {}*{}".format(fw, fh))
 # Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('sample4.avi',fourcc, 20.0, (fw,fh))
-
+#fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#out = cv2.VideoWriter('sample4.avi',fourcc, 20.0, (fw,fh))
+picture_num = 0
 if(cap.isOpened()):
     while(1):
         ret, frame = cap.read()
         if ret==True:
             frame = cv2.flip(frame,0)
-
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             # write the flipped frame
-            out.write(frame)
+            # out.write(frame)
 
- #           cv2.imshow('frame',frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.imshow('frame',gray)
+
+            ch = 0xFF & cv2.waitKey(1)
+            if ch == ord('q'):
                 break
+            elif ch == ord('s'):
+                picture_name = "frame" + str(picture_num) + '.png'
+                cv2.imwrite(picture_name,gray)
+                picture_num = picture_num + 1
+                print('Save frame: {}'.format(picture_name))
         else:
             break
 else:
     print('No camera!')
 # Release everything if job is finished
 cap.release()
-out.release()
+#out.release()
 cv2.destroyAllWindows()
