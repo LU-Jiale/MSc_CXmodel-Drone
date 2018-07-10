6b1e154949c914dfe0ba8015b6249242d36334fd
Jiale-Lu <lulujiale@outlook.com>
2018-07-10 17:57:13 +0100

    update code

--- code/CX_model/forward_optical.py
+++ code/CX_model/forward_optical.py
@@ -90,8 +90,9 @@ plt.ion()
 fig, (ax1, ax2) = plt.subplots(2, sharey=True)
 ax1.set(title='speed', ylabel='left')
 ax2.set(xlabel='time (s)', ylabel='right')
-speed_left = np.zeros_like(row)
-speed_right = np.zeros_like(row)
+row = np.linspace(0, 100, num=100, endpoint=False)
+speed_left = np.zeros(100)
+speed_right = np.zeros(100)
 plt.show()
 
 ret, frame1 = cap.read()
@@ -105,6 +106,8 @@ while(1):
     start_time = time.time()
     # Image processing, compute optical flow
     ret, frame2 = cap.read()
+    ret, frame2 = cap.read()
+    ret, frame2 = cap.read()
 
     frame2 = cv2.flip(frame2,1)
     temp = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
@@ -112,24 +115,28 @@ while(1):
     flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
     hori_flow = flow[:,:,0]
     
-    left_frame_shift = 162 - 83
+    left_frame_shift = 162 - 90
     frame_left = np.roll(hori_flow, -left_frame_shift, axis=1)
     frame_left[:,fw-left_frame_shift:fw-1] = 0
     right_frame_shift = 232 - 162
     frame_right = np.roll(hori_flow, right_frame_shift, axis=1)
     frame_right[:,0:right_frame_shift-1] = 0
-    #elapsed_time = time.time() - start_time
-    mag = np.abs(hori_flow)
-    mag[mag < 0.5] = 0
+    elapsed_time = time.time() - start_time
+    # left speed
+    mag = np.abs(frame_left)
+    mag[mag < 1.0] = 0
     mag[mag > 0.0] = 1.0
     count = np.sum(mag)
-
-    mag = np.abs(frame_left)
-    weight = mag/(count + 10000)
+    weight = mag/(elapsed_time*(count + 10000))
     sl = np.sum(frame_left * match_filter*weight)
+    # right speed
     mag = np.abs(frame_right)
-    weight = mag/(count + 10000)
+    mag[mag < 1.0] = 0
+    mag[mag > 0.0] = 1.0
+    count = np.sum(mag)
+    weight = mag/(elapsed_time*(count + 10000))
     sr = np.sum(frame_right * match_filter*weight)
+
     # visulize computed speed 
     speed_left = np.roll(speed_left, 1)
     speed_left[0] = (sl) #+ np.sum(speed_left[1:3]))/4
@@ -137,8 +144,8 @@ while(1):
     speed_right[0] = (sr) #+ np.sum(speed_right[1:3]))/4
     ax1.clear()
     ax2.clear()
-    ax1.plot(row, speed_left, 'k-')
-    ax2.plot(row, speed_right, 'k-')
+    ax1.plot(row, speed_left, 'r-')
+    ax1.plot(row, speed_right, 'k-')
     plt.draw()
 
     # show video
@@ -151,7 +158,7 @@ while(1):
         if cv2.waitKey(5) & 0xFF == ord('q'):
             break
 
-    print(np.sum(mag),np.average(mag))
+    print(count,np.average(mag))
     prvs = next
 
 cap.release()
