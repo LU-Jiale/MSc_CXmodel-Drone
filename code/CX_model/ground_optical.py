65f166216111c03b526f2a791def941bdabd1198
Jiale-Lu <lulujiale@outlook.com>
2018-07-12 04:31:06 +0100

    Update code

--- code/CX_model/ground_optical.py
+++ code/CX_model/ground_optical.py
@@ -4,13 +4,24 @@ import central_complex
 import cx_rate
 import cx_basic
 import time
+import sys
+import dronekit
+import os
+import camera_calibration
 import matplotlib.pyplot as plt
-from scipy import signal
+import matplotlib.animation as animation
+
+home = os.environ['HOME']
+if home.split('/')[-1] == 'pi':
+    show_frames = False
+else:
+    show_frames = True
 
 def draw_flow(img, flow, step=10):
     h, w = img.shape[:2]
     y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1)
-    fx, fy = flow[y,x].T *10
+    fx, fy = flow[y,x].T *3
+    fy[:] = 0
     lines = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
     lines = np.int32(lines + 0.5)
     vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
@@ -29,9 +40,8 @@ def update_cells(heading, velocity, tb1, memory, cx, filtered_steps=0.0):
     tb1 = cx.tb1_output(cl1, tb1)
 
     # Speed
-    flow = cx.get_flow(heading, velocity, filtered_steps)
-    tn1 = cx.tn1_output(flow)
-    tn2 = cx.tn2_output(flow)
+    tn1 = cx.tn1_output(velocity)
+    tn2 = cx.tn2_output(velocity)
 
     # Update memory for distance just travelled
     memory = cx.cpu4_update(memory, tb1, tn1, tn2)
@@ -41,7 +51,7 @@ def update_cells(heading, velocity, tb1, memory, cx, filtered_steps=0.0):
     cpu1 = cx.cpu1_output(tb1, cpu4)
     motor = cx.motor_output(cpu1)
     return tl2, cl1, tb1, tn1, tn2, memory, cpu4, cpu1, motor
-'''
+
 # connect to PX4
 try:
     drone = dronekit.connect('/dev/ttyAMA0', baud = 57600, heartbeat_timeout=15)
@@ -51,7 +61,7 @@ except dronekit.APIException:
 # Other error
 except:
     print 'Some other error!'
-'''
+
 
 # initialize CX model
 cx = cx_rate.CXRate(noise = 0)
@@ -61,77 +71,96 @@ memory = 0.5 * np.ones(central_complex.N_CPU4)
 
 
 # initialize camera
-cap = cv2.VideoCapture('sample2.avi')
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,200)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT,130)
-fw = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-fh = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fw_quarter = int(fw/4)
+cap = cv2.VideoCapture(sys.argv[1])
+#cap.set(cv2.CAP_PROP_FRAME_WIDTH,200)
+#cap.set(cv2.CAP_PROP_FRAME_HEIGHT,130)
+#fw = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+#fh = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+ret, frame1 = cap.read()
+#frame1 = cv2.resize(frame1, (0,0), fx=0.25, fy=0.25)
+temp = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+prvs = camera_calibration.undistort(temp, 1.0)
+(fh, fw) = prvs.shape
 print("Frame size: {0}*{1}".format(fw,fh))
 
-# filter for speed retrieval
-left_filter = np.zeros([fh,fw,2])
-right_filter = np.zeros([fh,fw,2])
-left_filter[:,:,0] = np.cos(3*np.pi/4)
-left_filter[:,:,1] = np.sin(3*np.pi/4)
-right_filter[:,:,0] = np.cos(np.pi/4)
-right_filter[:,:,1] = np.sin(np.pi/4)
 # visulize computed speed 
 plt.ion()
-
 fig, (ax1, ax2) = plt.subplots(2, sharey=True)
 ax1.set(title='speed', ylabel='left')
 ax2.set(xlabel='time (s)', ylabel='right')
-speed_left = np.zeros(300)
-speed_right = np.zeros(300)
-row = np.arange(300)
+x_axis = np.linspace(0, 100, num=100, endpoint=False)
+speed_left = np.zeros(100)
+speed_right = np.zeros(100)
 plt.show()
 
-ret, frame1 = cap.read()
-prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
-hsv = np.zeros_like(frame1)
-hsv[...,1] = 255
-
+# filter for speed retrieval
+row = np.linspace(0, fw, num=fw, endpoint=False)
+match_filter = np.sin((row/fw -0.5)*0.822*np.pi)
 
 while(1):
     start_time = time.time()
     # Image processing, compute optical flow
     ret, frame2 = cap.read()
-    next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+    ret, frame2 = cap.read()
+    #frame2 = cv2.resize(frame2, (0,0), fx=0.25, fy=0.25)
+    temp = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+    next = camera_calibration.undistort(temp, 1.0)
     flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-    mag = np.sqrt(np.power(flow[:,:,0],2) + np.power(flow[:,:,1],2))
-    weight = np.zeros_like(flow)
-    weight[:,:,0] = mag/(sum(mag))
-    weight[:,:,1] = mag/(sum(mag))
-    #elapsed_time = time.time() - start_time
-    #mag = mag.flatten()
-    #count = [i for i in mag if i >= 2]
-
-    sl = np.sum((flow * left_filter) * weight)
-    sr = np.sum((flow * right_filter) * weight)
+    hori_flow = flow[:,:,0]
+    
+    left_frame_shift = fw/4
+    frame_left = np.roll(hori_flow, -left_frame_shift, axis=1)
+    frame_left[:,fw-left_frame_shift:fw-1] = 0
+    right_frame_shift = fw/4
+    frame_right = np.roll(hori_flow, right_frame_shift, axis=1)
+    frame_right[:,0:right_frame_shift-1] = 0
+    elapsed_time = time.time() - start_time
+    # left speed
+    mag = np.abs(frame_left)
+    mag[mag < 1.0] = 0
+    mag[mag > 0.0] = 1.0
+    count = np.sum(mag)
+    weight = mag/(elapsed_time*(count + 10000))
+    sl = np.sum(frame_left * (match_filter)*weight)
+    # right speed
+    mag = np.abs(frame_right)
+    mag[mag < 1.0] = 0
+    mag[mag > 0.0] = 1.0
+    count = np.sum(mag)
+    weight = mag/(elapsed_time*(count + 10000))
+    sr = np.sum(frame_right * (match_filter)*weight)
 
     # visulize computed speed 
-    box_filter = np.array([1,1,1,1,1,1,1])/7.0
     speed_left = np.roll(speed_left, 1)
-    speed_left[0] = (sl + np.sum(speed_left[1:3]))/4
+    speed_left[0] = (sl) # + np.sum(speed_left[1:3]))/4
     speed_right = np.roll(speed_right, 1)
-    speed_right[0] = sr
-    
+    speed_right[0] = (sr) # + np.sum(speed_right[1:3]))/4
     ax1.clear()
     ax2.clear()
-    #speed_left =signal.convolve(speed_left,box_filter,mode='same')
-    ax1.plot(row, speed_left, 'k-')
-    #speed_right =signal.convolve(speed_right,box_filter,mode='same')
-    ax2.plot(row, speed_right, 'k-')
+    ax1.plot(x_axis, speed_left, 'r-')
+    ax1.plot(x_axis, speed_right, 'k-')
     plt.draw()
-    #print(len(mag))
-    # show video
-    cv2.imshow('vedio', cv2.resize(draw_flow(next, flow), (0,0), fx=2, fy=2))
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
-    
+    # show video
+    #leftF = np.roll(next, -fw/4, axis=1)
+    #leftFlow = np.roll(flow, -fw/4, axis=1)
+
+    # updare cx_neurons
+    velocity = np.array([sl, sr])
+    tl2, cl1, tb1, tn1, tn2, memory, cpu4, cpu1, motor = update_cells(
+            heading=0, velocity=velocity, tb1=tb1, memory=memory, cx=cx)
+
+    # show frames if not in rapberry pi
+    if True:
+        cv2.imshow('vedio', cv2.resize(draw_flow(next, flow), (0,0), fx=2, fy=2))
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            break
+    angle, distance = cx.decode_cpu4(cpu4)
+    print((angle/np.pi) * 180, distance)
+
     prvs = next
+
 cap.release()
 cv2.destroyAllWindows()
 
--- code/vector_visulization.py
+++ code/vector_visulization.py
@@ -1,33 +1,38 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 from numpy import ma
+from numpy import linalg as LA
 
-X, Y = np.meshgrid(np.arange(0, 2 * np.pi, .2), np.arange(0, 2 * np.pi, .2))
-U = np.cos(X)
-V = np.sin(Y)
+def draw_flow(img, flow, step=20):
+    h, w = img.shape[:2]
+    y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1)
+    fx, fy = flow[y,x].T *18
+    lines = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
+    lines = np.int32(lines + 0.5)
+    vis = img
+    cv2.polylines(vis, lines, 0, (0, 255, 0))
+    #print(flow.shape)
+    #for (x1, y1), (x2, y2) in lines:
+        #cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
+    return vis
 
-plt.figure()
-plt.title('Arrows scale with plot width, not view')
-Q = plt.quiver(X, Y, U, V, units='width')
-qk = plt.quiverkey(Q, 0.9, 0.9, 2, r'$2 \frac{m}{s}$', labelpos='E',
-                   coordinates='figure')
+vertical_views = (np.arange(244, dtype=float)-122.0)/244.0*(100.0/180.0*np.pi)
+horizontal_views = (np.arange(324, dtype=float)-162.0)/324.0*(100.0/180.0*np.pi)
 
-plt.figure()
-plt.title("pivot='mid'; every third arrow; units='inches'")
-Q = plt.quiver(X[::3, ::3], Y[::3, ::3], U[::3, ::3], V[::3, ::3],
-               pivot='mid', units='inches')
-qk = plt.quiverkey(Q, 0.9, 0.9, 1, r'$1 \frac{m}{s}$', labelpos='E',
-                   coordinates='figure')
-plt.scatter(X[::3, ::3], Y[::3, ::3], color='r', s=5)
+D = np.zeros([244,324,3])
+for i in range(244):
+    for j in range(324):
+        D[i,j]=np.array([np.tan(vertical_views[i]), np.tan(horizontal_views[j]),-1])
+        D[i,j] /= LA.norm(D[i,j])
 
-plt.figure()
-plt.title("pivot='tip'; scales with x view")
-M = np.hypot(U, V)
-Q = plt.quiver(X, Y, U, V, M, units='x', pivot='tip', width=0.022,
-               scale=1 / 0.15)
-qk = plt.quiverkey(Q, 0.9, 0.9, 1, r'$1 \frac{m}{s}$', labelpos='E',
-                   coordinates='figure')
-plt.scatter(X, Y, color='k', s=5)
+a = np.array([0, 1, 0])
+matched_filter = np.cross(np.cross(D,a),D)[:,:,0:2]
 
-plt.show()
+print(matched_filter.shape)
 
+img = np.ones([244,324,3])
+vector_map = draw_flow(img, matched_filter)
+cv2.imshow('vector_map', vector_map)
+cv2.waitKey()
+cv2.destroyAllWindows()
