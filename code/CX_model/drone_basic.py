896d5df6a578e71b7eff90df2f4af59074bb9d74
Jiale <lulujiale@outlook.com>
2018-07-15 18:12:44 +0000

    from pi3

--- code/CX_model/drone_basic.py
+++ code/CX_model/drone_basic.py
@@ -1,3 +1,4 @@
+import time
 import dronekit
 from dronekit import VehicleMode
 
@@ -48,7 +49,7 @@ def arm_and_takeoff(vehicle, aTargetAltitude):
             return "Arm motors failed! Mission cancelled."
 
     # Taking off
-    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+    #vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
 
     # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
     #  after Vehicle.simple_takeoff will execute immediately).
@@ -65,7 +66,7 @@ def arm_and_takeoff(vehicle, aTargetAltitude):
             return 'Interrupt from controller, Mission cancelled.'
     return "Take off finished"
 
-def arm():
+def arm(vehicle):
     """
     Arms vehicle 
     """
@@ -76,7 +77,7 @@ def arm():
             print " Waiting for vehicle to initialise..."
     	    if vehicle.is_armable:
                 break
-            time.sleep(5)    
+            time.sleep(5)
         if not vehicle.is_armable:
             print "Initialisation failed! Mission cancelled."
             return "Initialisation failed! Mission cancelled."
@@ -85,24 +86,21 @@ def arm():
     vehicle.mode = VehicleMode("POSCTL")
     time.sleep(5)
     if not vehicle.mode == VehicleMode("POSCTL"):
-        
         print "Mode setup failed! Takeoff cancelled."
         return "Mode setup failed! Takeoff cancelled."
     else:
         vehicle.armed = True
 
-    # Confirm vehicle armed before attempting to do other things   
+    # Confirm vehicle armed before attempting to do other thing
     if not vehicle.armed:
         for i in range(6):
             print " Waiting for vehicle to arm..."
     	    if vehicle.armed:
                 break
-            time.sleep(5)    
+            time.sleep(5)
         if not vehicle.armed:
             print "Arm motors failed! Mission cancelled."
             return "Arm motors failed! Mission cancelled."
-    
-    return "Armed successfully
 
-    
+    return "Armed successfully"
 
