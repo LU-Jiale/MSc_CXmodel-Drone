0f32993d540869340467af777c8c5377771337fc
Jiale <lulujiale@outlook.com>
2018-07-20 11:29:52 +0000

    From pi3

---
+++ code/dronekit_modetest.py
@@ -0,0 +1,64 @@
+import dronekit
+import socket
+import exceptions
+import time
+import cv2
+from dronekit import connect, VehicleMode, LocationGlobalRelative, LocationGlobal, Command
+from CX_model.drone_basic import arm, arm_and_takeoff, download_mission, adds_square_mission, PX4setMode
+from pymavlink import mavutil
+
+# Try to connect to PX4
+try:
+    vehicle = dronekit.connect('/dev/ttyAMA0', baud=921600, wait_ready=True)
+# Bad TCP connection
+except socket.error:
+    print 'No server exists!'
+# Bad TTY connection
+except exceptions.OSError as e:
+    print 'No serial exists!'
+# API Error
+except dronekit.APIException:
+    print 'Timeout!'
+# Other error
+except:
+    print 'Some other error!'
+
+# Get all vehicle attributes (state)
+print "\nGet all vehicle attribute values:"
+print "   Supports commanding attitude offboard: %s" % vehicle.capabilities.set_attitude_target
+print "   Supports commanding position and velocity targets in local NED frame: %s" % vehicle.capabilities.set_attitude_target_local_ned
+print "   Supports set position + velocity targets in global scaled integers: %s" % vehicle.capabilities.set_altitude_target_global_int
+print " Global Location: %s" % vehicle.location.global_frame
+print " Global Location (relative altitude): %s" % vehicle.location.global_relative_frame
+print " Local Location: %s" % vehicle.location.local_frame
+print " Attitude: %s" % vehicle.attitude
+print " Velocity: %s" % vehicle.velocity
+print " GPS: %s" % vehicle.gps_0
+print " Battery: %s" % vehicle.battery
+print " EKF OK?: %s" % vehicle.ekf_ok
+print " Last Heartbeat: %s" % vehicle.last_heartbeat
+print " Heading: %s" % vehicle.heading
+print " Is Armable?: %s" % vehicle.is_armable
+print " System status: %s" % vehicle.system_status.state
+print " Groundspeed: %s" % vehicle.groundspeed    # settable
+print " Airspeed: %s" % vehicle.airspeed    # settable
+print " Mode: %s" % vehicle.mode.name    # settable
+print " Armed: %s" % vehicle.armed    # settable
+
+while not vehicle.ekf_ok:
+    print "EFK is not intialised properly"
+    time.sleep(2)
+
+
+if vehicle:
+    vehicle.mode = VehicleMode("POSCTL")
+    print vehicle.mode.name
+    time.sleep(2)
+    vehicle.mode = VehicleMode("MISSION")
+    print vehicle.mode.name
+    time.sleep(2)
+    arm(vehicle)
+    vehicle.mode = VehicleMode("OFFBOARD")
+    print vehicle.mode.name
+    vehicle.armed = false
+    vehicle.close()
